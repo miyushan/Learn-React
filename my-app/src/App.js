24613b4246199081fd6fc06f7fc88bf534ed2892
miyushan <smiushan2015@gmail.com>
2021-05-16 13:58:07 +0530

    Multiple components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,14 @@
 import './App.css';
+import Navbar from'./Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const person = { name:'miyushan', age:23};
-  const link = "http://www.google.com";
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{person.age}</p>
-        <p>{"Hello Ninga"}</p>
-        <p>{ [1,2,3,4,5]}</p>
-        <p>{ Math.random()*10 }</p>
-        <a href={link}>Click to google</a>
+        <Home />
       </div>
     </div>
   );
