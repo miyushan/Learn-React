1a7fbb9c04b9e1c36de8c0aa8f385d36dae7ca9d
miyushan <smiushan2015@gmail.com>
2021-05-16 13:44:46 +0530

    Dynamic values in templates

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ function App() {
   const title = 'Welcome to the new blog';
   const likes = 50;
   const person = { name:'miyushan', age:23};
+  const link = "http://www.google.com";
 
   return (
     <div className="App">
@@ -11,6 +12,10 @@ function App() {
         <h1>{title}</h1>
         <p>Liked {likes} times</p>
         <p>{person.age}</p>
+        <p>{"Hello Ninga"}</p>
+        <p>{ [1,2,3,4,5]}</p>
+        <p>{ Math.random()*10 }</p>
+        <a href={link}>Click to google</a>
       </div>
     </div>
   );
