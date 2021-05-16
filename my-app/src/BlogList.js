f85423d7f7c4e658a4aa7f8d7f3d195a960333a5
miyushan <smiushan2015@gmail.com>
2021-05-16 20:01:06 +0530

    Props 1

---
+++ my-app/src/BlogList.js
@@ -0,0 +1,19 @@
+const BlogList = (props) => {
+
+    const blogs = props.blogs;
+    const word = props.word;
+
+    return ( 
+        <div className="blog-list">
+            <h2>{word}</h2>
+            {blogs.map((blog) =>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.titile}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+    
+export default BlogList;
